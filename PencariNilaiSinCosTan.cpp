cf6f982fda63e0ff1e91597ada740232487f9a1b
NajamuddinFawwazHaq <najamuddinfawwazhaq2005@mail.ugm.ac.id>
2024-09-12 21:31:50 +0700

    Update 1

--- PencariNilaiSinCosTan.cpp
+++ PencariNilaiSinCosTan.cpp
@@ -6,32 +6,45 @@ using namespace std;
 
 int main () {
     int pilihan; //Pilihan untuk menentukan apa yang dicari
-    double nilai; //Variabel nilai saat diinput 
-    cout << "=========================================================" << endl; //Hiasan doang, biar bagus wkwkw
-    cout << "Selamat datang di dalam mesin pencarian nilai sin cos tan" << endl;
-    cout << "Pilih angka 1 untuk mencari nilai sin" << endl;
-    cout << "Pilih angka 2 untuk mencari nilai cos" << endl;
-    cout << "Pilih angka 3 untuk mencari nilai tan" << endl;
-    cout << endl << "Pilih opsi anda : ";
-    cin >> pilihan; //Input opsi pilihan
-    cout << endl << "Masukkan angka : ";
-    cin >> nilai; //Input nilai
+    double nilai; //Variabel nilai saat diinput
+    char ulang;
+    do
+    {
+        cout << "===============================================================" << endl;
+        cout << "## Selamat datang di dalam mesin pencarian nilai sin cos tan ##" << endl;
+        cout << "===============================================================" << endl;
+        cout << endl;
+        cout << "Pilih angka 1 untuk mencari nilai sin" << endl;
+        cout << "Pilih angka 2 untuk mencari nilai cos" << endl;
+        cout << "Pilih angka 3 untuk mencari nilai tan" << endl;
+        cout << endl << "Pilih opsi anda : ";
+        cin >> pilihan; //Input opsi pilihan
+        cout << endl << "Masukkan angka : ";
+        cin >> nilai; //Input nilai
 
-    if (pilihan == 1) 
-    {
-        cout << endl << "Nilai sin dari " << nilai << " adalah : " << sin(nilai) << endl; //Code
+        if (pilihan == 1) 
+        {
+            cout << endl << "Nilai sin dari " << nilai << " adalah : " << sin(nilai) << endl; //Code
+        }
+        else if (pilihan == 2) 
+        {
+            cout << endl << "Nilai cos dari " << nilai << " adalah : " << cos(nilai) << endl; //Code
+        }
+        else if (pilihan == 3)
+        {
+            cout << endl << "Nilai tan dari " << nilai << " adalah : " << tan(nilai) << endl; //Code
+        }
+        else
+        {
+            cout << endl << "Error woy, suruh isi 1 sampai 3 aja malah milih yang lain. Di suruh milih aku atau dia, malah milih kamu" << endl;
+        } // Ceritanya sebel aja sih, suruh ngisi sesuai petunjuk malah nggak nurut
+        cout << endl;
+
+        cout << "Mau cari lagi? (y/t) = ";
+        cin >> ulang;
+        cout << endl;
     }
-    else if (pilihan == 2) 
-    {
-        cout << endl << "Nilai cos dari " << nilai << " adalah : " << cos(nilai) << endl; //Code
-    }
-    else if (pilihan == 3)
-    {
-        cout << endl << "Nilai tan dari " << nilai << " adalah : " << tan(nilai) << endl; //Code
-    }
-    else
-    {
-        cout << endl << "Error woy, suruh isi 1 sampai 3 aja malah milih yang lain. Di suruh milih aku atau dia, malah milih kamu" << endl;
-    } // Ceritanya sebel aja sih, suruh ngisi sesuai petunjuk malah nggak nurut
+    while (ulang!= 't');
+    cout << "Semangat belajarnya ..." << endl;
     return 0;
 }
